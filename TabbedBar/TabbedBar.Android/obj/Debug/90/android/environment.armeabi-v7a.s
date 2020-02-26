4b810c707d46b96d8a600a43c9f180db00daf1a2
RogelioCordova <55772023+droozyson@users.noreply.github.com>
2020-02-25 16:59:43 -0800

    third commit, changed icons and made the display alerts only display once.

--- TabbedBar/TabbedBar.Android/obj/Debug/90/android/environment.armeabi-v7a.s
+++ TabbedBar/TabbedBar.Android/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -82,7 +82,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",%progbits,1
 	.type	.L.str.8, %object
 .L.str.8:
-	.asciz	"7c9d66b2-b6ce-4fdb-bfc4-ecceea81be55"
+	.asciz	"b7fbd144-8948-48ad-8f1c-2660bfb38466"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",%progbits,1
 	.type	.L.str.9, %object
